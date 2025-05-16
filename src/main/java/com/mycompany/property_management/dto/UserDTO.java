3fe3af55448cf8eb455ef670df05789fcf82c3d4
wolframe.haule <haulewolfram@gmail.com>
2025-05-16 09:59:42 +0300

    Implemented logging with logback and slf4j

--- src/main/java/com/mycompany/property_management/dto/UserDTO.java
+++ src/main/java/com/mycompany/property_management/dto/UserDTO.java
@@ -3,8 +3,13 @@ package com.mycompany.property_management.dto;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+import org.apache.logging.log4j.message.Message;
 
 @Getter
 @Setter
@@ -13,7 +18,12 @@ import lombok.Setter;
 public class UserDTO {
     private Long id;
     private String ownerName;
+    @NotNull(message = "Owner Email is required")
+    @NotEmpty(message = "Owner email can not be Empty")
+    @Size(min=1, max=50, message="Email should be 1 to 50 character in length")
     private String ownerEmail;
     private String phoneNumber;
+    @NotNull(message = "Password is mandatory")
+    @NotEmpty(message = "password can not be empty")
     private String password;
 }
--- src/main/java/com/mycompany/property_management/exception/CustomExceptionHandler.java
+++ src/main/java/com/mycompany/property_management/exception/CustomExceptionHandler.java
@@ -1,17 +1,44 @@
 package com.mycompany.property_management.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @ControllerAdvice
 public class CustomExceptionHandler {
+    private final Logger logger= LoggerFactory.getLogger(this.getClass());
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<ErrorModel>> handleFieldValidation(MethodArgumentNotValidException manv){
+        List<ErrorModel> errorModelList=new ArrayList<>();
+        List<FieldError>fieldErrorList=manv.getBindingResult().getFieldErrors();
+        ErrorModel errorModel=null;
+        for(FieldError fe:fieldErrorList){
+            logger.debug("Inside Field Validation: {} - {}", fe.getField(), fe.getDefaultMessage());
+            logger.info("Inside Field Validation: {} - {}", fe.getField(), fe.getDefaultMessage());
+            errorModel=new ErrorModel();
+            errorModel.setCode(fe.getField());
+            errorModel.setMessage(fe.getDefaultMessage());
+            errorModelList.add(errorModel);
+        }
+        return new ResponseEntity<List<ErrorModel>>(errorModelList, HttpStatus.BAD_REQUEST);
+    }
     @ExceptionHandler(BusinessException.class)
-    public ResponseEntity handleBusinessException(BusinessException bex){
-        System.out.println("Business Exception is thrown");
+    public ResponseEntity<List<ErrorModel>> handleBusinessException(BusinessException bex){
+        for(ErrorModel em: bex.getErrors()){
+            logger.debug("Business Exception is thrown level-as-debug : {} - {}", em.getCode(), em.getMessage());
+            logger.info("Business Exception is thrown level-as-info: {} - {}", em.getCode(), em.getMessage());
+            logger.warn("Business Exception is thrown level-as-warn: {} - {}", em.getCode(), em.getMessage());
+            logger.error("Business Exception is thrown level-as-error: {} - {}", em.getCode(), em.getMessage());
+        }
+
         return new ResponseEntity<List<ErrorModel>>(bex.getErrors(), HttpStatus.BAD_REQUEST);
 
     }
